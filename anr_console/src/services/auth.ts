95be390e4abd71dec2ce5feb68259480a8d8bb4e
Alexandre Nunes Reys <anr.alexandre@gmail.com>
2023-09-18 09:05:22 -0300

    add axios to acess login endpoint

--- anr_console/src/services/auth.ts
+++ anr_console/src/services/auth.ts
@@ -1,25 +1,21 @@
-import { v4 as uuid } from 'uuid';
+import axios from 'axios';
+
+const delay = (amount = 750) => new Promise( resolve => setTimeout(resolve, amount));
 
 type SignInRequestData = {
   email: string;
   password: string;
 };
 
-const delay = (amount = 750) => new Promise( resolve => setTimeout(resolve, amount));
-
 export async function signInRequest(data: SignInRequestData) {
-  await delay()
-
-  return {
-    token: uuid(),
-    user: {
-      name: 'Alexandre Nunes Reys',
-      email: 'anr.alexandre@gmail.com',
-      avatar_url: 'https://github.com/alexandreReys.png',
-      password: '',
-    }
-  }
+  const response = await axios.post('http://localhost:3000/auth/v1/login', data);
+  return response.data;
 }
+
+// export async function recoverUserInformation() {
+//   const response = await axios.get('http://localhost:3000/api/v1/users');
+//   return response.data;
+// }
 
 export async function recoverUserInformation() {
   await delay()
@@ -33,4 +29,41 @@ export async function recoverUserInformation() {
     }
   }
 }
+
+
+// import { v4 as uuid } from 'uuid';
+
+// type SignInRequestData = {
+//   email: string;
+//   password: string;
+// };
+
+// const delay = (amount = 750) => new Promise( resolve => setTimeout(resolve, amount));
+
+// export async function signInRequest(data: SignInRequestData) {
+//   await delay()
+
+//   return {
+//     token: uuid(),
+//     user: {
+//       name: 'Alexandre Nunes Reys',
+//       email: 'anr.alexandre@gmail.com',
+//       avatar_url: 'https://github.com/alexandreReys.png',
+//       password: '',
+//     }
+//   }
+// }
+
+// export async function recoverUserInformation() {
+//   await delay()
+
+//   return {
+//     user: {
+//       name: 'Alexandre Nunes Reys',
+//       email: 'anr.alexandre@gmail.com',
+//       avatar_url: 'https://github.com/alexandreReys.png',
+//       password: '',
+//     }
+//   }
+// }
 
