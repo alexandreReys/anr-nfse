b90742f11c9899bef7474e305d1ce72b67c212cd
Alexandre Nunes Reys <anr.alexandre@gmail.com>
2023-09-24 11:44:45 -0300

    change API port to 3000

--- anr_console/src/services/axios.ts
+++ anr_console/src/services/axios.ts
@@ -5,13 +5,14 @@ export function getAPIClient(ctx?: any) {
   const { 'nextauth.token': token } = parseCookies(ctx);
 
   const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'http://localhost:3000'
   })
   
-  api.interceptors.request.use(config => {
-    console.log(config);
-    return config;
-  })
+  // This allow us to intercept all the requests
+  // api.interceptors.request.use(config => {
+  //   console.log(config);
+  //   return config;
+  // })
   
   if (token) {
     api.defaults.headers['Authorization'] = `Bearer ${token}`;
