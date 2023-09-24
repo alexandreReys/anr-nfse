621f8ec6845eec88f009bddfba238848d25bc332
Alexandre Nunes Reys <anr.alexandre@gmail.com>
2023-09-24 11:42:26 -0300

    add store provider

--- anr_console/src/pages/_app.tsx
+++ anr_console/src/pages/_app.tsx
@@ -1,12 +1,18 @@
+// _app.tsx
+import React from 'react';
 import 'tailwindcss/tailwind.css'
 import { AuthProvider } from '../context/authContext/AuthContext'
+import { Provider } from 'react-redux';
+import store from '../store';
 
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </Provider>
   )
 }
