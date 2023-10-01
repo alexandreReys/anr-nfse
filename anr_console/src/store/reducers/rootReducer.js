cdee2c23ff4db9acd89dafb3648252c08cfceec3
Alexandre Nunes Reys <anr.alexandre@gmail.com>
2023-10-01 14:34:50 -0300

    add organizationsReducer

--- anr_console/src/store/reducers/rootReducer.js
+++ anr_console/src/store/reducers/rootReducer.js
@@ -1,9 +1,11 @@
 // rootReducer.js
 import { combineReducers } from 'redux';
-import usersReducer from './userSlice';
+import usersReducer from './usersSlice';
+import organizationsReducer from './organizationsSlice';
 
 const rootReducer = combineReducers({
   users: usersReducer,
+  organizations: organizationsReducer,
 });
 
 export default rootReducer;
