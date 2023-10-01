7c7629c18f60c02ccaa15cfaa5b11c25fb1ae4b3
Alexandre Nunes Reys <anr.alexandre@gmail.com>
2023-10-01 14:24:36 -0300

    refactor

--- anr_api/micro-services/main/src/config/errors-mapped.js
+++ anr_api/micro-services/main/src/config/errors-mapped.js
@@ -3,383 +3,98 @@ class ErrorsMapped {}
 //prettier-ignore
 ErrorsMapped.Custom = {
   message: '',
-  code: 8,
-  status: 403,
+  code: 1,
+  status: 400,
   errorKey: ''
 };
-ErrorsMapped.DontHaveError = {
-  message: 'DontHaveErrorException',
-  code: 0,
-  status: 403,
-  errorKey: 'DontHaveErrorException',
-};
-ErrorsMapped.InvalidCredentials = {
-  message: 'InvalidCredentialsException',
+ErrorsMapped.RequestNotFound = {
+  message: 'RequestNotFoundException',
   code: 1,
   status: 404,
+  errorKey: 'RequestNotFoundException',
+};ErrorsMapped.InvalidCredentials = {
+  message: 'InvalidCredentialsException',
+  code: 2,
+  status: 401,
   errorKey: 'InvalidCredentialsException',
 };
 ErrorsMapped.EmailInUse = {
   message: 'EmailInUseException',
-  code: 2,
-  status: 403,
+  code: 3,
+  status: 409,
   errorKey: 'EmailInUseException',
 };
-ErrorsMapped.NicknameInUse = {
-  message: 'NicknameInUseException',
-  code: 3,
-  status: 403,
-  errorKey: 'NicknameInUseException',
+ErrorsMapped.EmptyEmail = {
+  message: 'EmptyEmailException',
+  code: 4,
+  status: 400,
+  errorKey: 'EmptyEmailException',
 };
 ErrorsMapped.InvalidEmail = {
   message: 'InvalidEmailException',
-  code: 4,
-  status: 403,
+  code: 5,
+  status: 400,
   errorKey: 'InvalidEmailException',
 };
 ErrorsMapped.InvalidPassword = {
   message: 'InvalidPasswordException',
-  code: 5,
-  status: 403,
-  errorKey: 'InvalidPasswordException',
-};
-ErrorsMapped.InvalidNickname = {
-  message: 'InvalidNicknameException',
   code: 6,
-  status: 403,
-  errorKey: 'InvalidNicknameException',
-};
-ErrorsMapped.NointernetConnection = {
-  message: 'NointernetConnectionException',
-  code: 7,
-  status: 403,
-  errorKey: 'NointernetConnectionException',
-};
-ErrorsMapped.UndefinedError = {
-  message: 'UndefinedErrorException',
-  code: 8,
-  status: 403,
-  errorKey: 'UndefinedErrorException',
-};
-ErrorsMapped.RequestNotFound = {
-  message: 'RequestNotFoundException',
-  code: 9,
-  status: 404,
-  errorKey: 'RequestNotFoundException',
-};
-ErrorsMapped.NoPermission = {
-  message: 'NoPermissionException',
-  code: 10,
-  status: 403,
-  errorKey: 'NoPermissionException',
+  status: 401,
+  errorKey: 'InvalidPasswordException',
 };
 ErrorsMapped.RecordNotExist = {
   message: 'RecordNotExistException',
-  code: 11,
+  code: 7,
   status: 404,
   errorKey: 'RecordNotExistException',
 };
 ErrorsMapped.TokenExpired = {
   message: 'TokenExpiredException',
-  code: 12,
-  status: 403,
+  code: 8,
+  status: 401,
   errorKey: 'TokenExpiredException',
 };
 ErrorsMapped.AccessDenied = {
   message: 'AccessDeniedException',
-  code: 13,
+  code: 9,
   status: 403,
   errorKey: 'AccessDeniedException',
 };
-ErrorsMapped.NoRoomPermission = {
-  message: 'NoRoomPermissionException',
-  code: 14,
-  status: 403,
-  errorKey: 'NoRoomPermissionException',
-};
-ErrorsMapped.InvalidPasswordRoom = {
-  message: 'InvalidPasswordRoomException',
-  code: 15,
-  status: 403,
-  errorKey: 'InvalidPasswordRoomException',
-};
-ErrorsMapped.EmptyNameSkin = {
-  message: 'EmptyNameSkinException',
-  code: 16,
-  status: 403,
-  errorKey: 'EmptyNameSkinException',
-};
-ErrorsMapped.SkinAlreadyExists = {
-  message: 'SkinAlreadyExistsException',
-  code: 17,
-  status: 403,
-  errorKey: 'SkinAlreadyExistsException',
-};
 ErrorsMapped.NoPermissionToDelete = {
   message: 'NoPermissionToDeleteException',
-  code: 18,
+  code: 10,
   status: 403,
   errorKey: 'NoPermissionToDeleteException',
 };
 ErrorsMapped.NoPermissionToUpdate = {
   message: 'NoPermissionToUpdateException',
-  code: 19,
+  code: 11,
   status: 403,
   errorKey: 'NoPermissionToUpdateException',
 };
-ErrorsMapped.EmptyNameEvent = {
-  message: 'EmptyNameEventException',
-  code: 20,
-  status: 403,
-  errorKey: 'EmptyNameEventException',
-};
-ErrorsMapped.EventAlreadyExists = {
-  message: 'EventAlreadyExistsException',
-  code: 21,
-  status: 403,
-  errorKey: 'EventAlreadyExistsException',
-};
-ErrorsMapped.NoPermissionToSubscribe = {
-  message: 'NoPermissionToSubscribeException',
-  code: 22,
-  status: 403,
-  errorKey: 'NoPermissionToSubscribeException',
-};
-ErrorsMapped.WorldAlreadyExists = {
-  message: 'WorldAlreadyExistsException',
-  code: 22,
-  status: 403,
-  errorKey: 'WorldAlreadyExistsException',
-};
-ErrorsMapped.InvalidCredentials = {
-  message: 'InvalidCredentialsException',
-  code: 23,
-  status: 404,
-  errorKey: 'InvalidCredentialsException',
-};
-ErrorsMapped.EmptyEmail = {
-  message: 'EmptyEmailException',
-  code: 24,
-  status: 403,
-  errorKey: 'EmptyEmailException',
-};
-ErrorsMapped.UserHaveEvents = {
-  message: 'UserHaveEventsException',
-  code: 25,
-  status: 403,
-  errorKey: 'UserHaveEventsException',
-};
-ErrorsMapped.EventNotFound = {
-  message: 'EventNotFoundException',
-  code: 26,
-  status: 403,
-  errorKey: 'EventNotFoundException',
-};
-ErrorsMapped.RoomNotFound = {
-  message: 'RoomNotFoundException',
-  code: 27,
-  status: 403,
-  errorKey: 'RoomNotFoundException',
-};
-ErrorsMapped.TicketAlreadyExists = {
-  message: 'TicketAlreadyExistsException',
-  code: 28,
-  status: 403,
-  errorKey: 'TicketAlreadyExistsException',
-};
-ErrorsMapped.EmptyTicketName = {
-  message: 'EmptyTicketNameException',
-  code: 29,
-  status: 403,
-  errorKey: 'EmptyTicketNameException',
-};
-ErrorsMapped.MapAlreadyExists = {
-  message: 'MapAlreadyExistsException',
-  code: 30,
-  status: 403,
-  errorKey: 'MapAlreadyExistsException',
-};
-ErrorsMapped.EmptyDisplayName = {
-  message: 'EmptyDisplayNameException',
-  code: 31,
-  status: 403,
-  errorKey: 'EmptyDisplayNameException',
-};
-ErrorsMapped.EmptyMapName = {
-  message: 'EmptyMapNameException',
-  code: 32,
-  status: 403,
-  errorKey: 'EmptyMapNameException',
-};
-ErrorsMapped.SpotAlreadyExists = {
-  message: 'SpotAlreadyExistsException',
-  code: 33,
-  status: 403,
-  errorKey: 'SpotAlreadyExistsException',
-};
-ErrorsMapped.EmptySpotName = {
-  message: 'EmptySpotNameException',
-  code: 34,
-  status: 403,
-  errorKey: 'EmptySpotNameException',
-};
-ErrorsMapped.MapNotFound = {
-  message: 'MapNotFoundException',
-  code: 35,
-  status: 403,
-  errorKey: 'MapNotFoundException',
-};
-ErrorsMapped.CouldNotDeleteImages = {
-  message: 'CouldNotDeleteImagesException',
-  code: 36,
-  status: 403,
-  errorKey: 'CouldNotDeleteImagesException',
-};
-ErrorsMapped.SpotsModelNotFound = {
-  message: 'SpotsModelNotFoundException',
-  code: 37,
-  status: 403,
-  errorKey: 'SpotsModelNotFoundException',
-};
 ErrorsMapped.RecordAlreadyExists = {
   message: 'RecordAlreadyExistsException',
-  code: 38,
-  status: 403,
+  code: 12,
+  status: 409,
   errorKey: 'RecordAlreadyExistsException',
 };
 ErrorsMapped.UserNotFound = {
   message: 'UserNotFoundException',
-  code: 39,
-  status: 403,
+  code: 13,
+  status: 404,
   errorKey: 'UserNotFoundException',
-};
-ErrorsMapped.RegionNotFound = {
-  message: 'RegionNotFoundException',
-  code: 40,
-  status: 403,
-  errorKey: 'RegionNotFoundException',
-};
-ErrorsMapped.EmptyRoomName = {
-  message: 'EmptyRoomNameException',
-  code: 41,
-  status: 403,
-  errorKey: 'EmptyRoomNameException',
-};
-ErrorsMapped.StreamServiceHaveEvents = {
-  message: 'StreamServiceHaveEventsException',
-  code: 42,
-  status: 403,
-  errorKey: 'StreamServiceHaveEventsException',
 };
 ErrorsMapped.BadRequest = {
   message: 'BadRequestException',
-  code: 43,
-  status: 403,
+  code: 14,
+  status: 400,
   errorKey: 'BadRequestException',
 };
-ErrorsMapped.PointInterestHaveEvents = {
-  message: 'PointInterestHaveEventsException',
-  code: 44,
-  status: 403,
-  errorKey: 'PointInterestHaveEventsException',
-};
-ErrorsMapped.AvatarHaveSkinsException = {
-  message: 'AvatarHaveSkinsException',
-  code: 45,
-  status: 403,
-  errorKey: 'AvatarHaveSkinsException',
-};
-ErrorsMapped.AvatarHaveSkinsModelException = {
-  message: 'AvatarHaveSkinsModelException',
-  code: 46,
-  status: 403,
-  errorKey: 'AvatarHaveSkinsModelException',
-};
-ErrorsMapped.CouldNotDeleteDefaultRoom = {
-  message: 'CouldNotDeleteDefaultRoomException',
-  code: 47,
-  status: 403,
-  errorKey: 'CouldNotDeleteDefaultRoomException',
-};
-ErrorsMapped.SkinModelHasSkins = {
-  message: 'SkinModelHasSkinsException',
-  code: 48,
-  status: 403,
-  errorKey: 'SkinModelHasSkinsException',
-};
-ErrorsMapped.EmptyDescription = {
-  message: 'EmptyDescriptionException',
-  code: 49,
-  status: 403,
-  errorKey: 'EmptyDescriptionException',
-};
-ErrorsMapped.InvalidPasswordConfirmation = {
-  message: 'InvalidPasswordConfirmationException',
-  code: 50,
-  status: 403,
-  errorKey: 'InvalidPasswordConfirmationException',
-};
-ErrorsMapped.ConfirmedPasswordDoNotMatch = {
-  message: 'ConfirmedPasswordDoNotMatchException',
-  code: 51,
-  status: 403,
-  errorKey: 'ConfirmedPasswordDoNotMatchException',
-};
-ErrorsMapped.MapHasRooms = {
-  message: 'MapHasRoomsException',
-  code: 52,
-  status: 403,
-  errorKey: 'MapHasRoomsException',
-};
-ErrorsMapped.InvalidProvider = {
-  message: 'InvalidProviderException',
-  code: 53,
-  status: 403,
-  errorKey: 'InvalidProviderException',
-};
-ErrorsMapped.IncompatibleMaps = {
-  message: 'IncompatibleMaps',
-  code: 54,
-  status: 403,
-  errorKey: 'IncompatibleMaps',
-};
-ErrorsMapped.EmptyList = {
-  message: 'EmptyList',
-  code: 55,
-  status: 403,
-  errorKey: 'EmptyList',
-};
-ErrorsMapped.RoomRequiredException = {
-  message: 'RoomRequiredException',
-  code: 56,
-  status: 403,
-  errorKey: 'RoomRequiredException',
-};
-ErrorsMapped.BadStructuredPosition = {
-  message: 'BadStructuredPosition',
-  code: 57,
-  status: 403,
-};
-ErrorsMapped.BadStructuredRotation = {
-  message: 'BadStructuredRotation',
-  code: 58,
-  status: 403,
-};
-ErrorsMapped.ListPOIsMustBeArray = {
-  message: 'ListPOIsMustBeArray',
-  code: 59,
-  status: 403,
-};
-ErrorsMapped.InvalidInvitationToken = {
-  message: 'InvalidInvitationToken',
-  code: 60,
-  status: 403,
-};
-ErrorsMapped.EmptyEvent = {
-  message: 'EmptyEventException',
-  code: 61,
-  status: 403,
-  errorKey: 'EmptyEventException',
+ErrorsMapped.OrganizationAlreadyExists = {
+  message: 'Empresa já cadastrada',
+  code: 15,
+  status: 409,
+  errorKey: 'OrganizationAlreadyExistsException',
 };
 
 exports.ErrorsMapped = ErrorsMapped;
