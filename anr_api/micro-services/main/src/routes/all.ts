d4284a94d60884a199bc30c8d1d28dbfbb918b3a
Alexandre Nunes Reys <anr.alexandre@gmail.com>
2023-10-24 14:11:16 -0300

    add nfse route

--- anr_api/micro-services/main/src/routes/all.ts
+++ anr_api/micro-services/main/src/routes/all.ts
@@ -5,6 +5,7 @@ import * as services from '../services';
 import authRouter from './auth';
 import usersRouter from './users';
 import organizationsRouter from './organizations';
+import nfseRouter from './nfse';
 
 const router = Router();
 
@@ -16,6 +17,7 @@ router.use((req, res, next) => {
 router.use('/auth/:version', authRouter);
 router.use('/api/:version/users', usersRouter);
 router.use('/api/:version/organizations', organizationsRouter);
+router.use('/api/:version/nfse', nfseRouter);
 router.use('/api/', require('./root'));
 router.use('/', require('./root'));
 
