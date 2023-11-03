fd24798e7c82b99a1606c7db54e9b3d52f7e10bc
Alexandre Nunes Reys <anr.alexandre@gmail.com>
2023-11-03 00:08:41 -0300

    add organizationId

--- anr_terraform/infra/services/dynamodb.tf
+++ anr_terraform/infra/services/dynamodb.tf
@@ -1,11 +1,31 @@
 resource "aws_dynamodb_table" "services" {
-  name     = "${var.environment}-services"
-  hash_key = "id"
+  name = "${var.environment}-services"
   # billing_mode = "PAY_PER_REQUEST"
+
+  hash_key  = "organizationId"
+  range_key = "id"
+
+  attribute {
+    name = "organizationId"
+    type = "S"
+  }
 
   attribute {
     name = "id"
     type = "S"
+  }
+
+  attribute {
+    name = "description"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "${var.environment}-services-description-gsi"
+    projection_type = "ALL"
+    hash_key        = "description"
+    write_capacity  = var.write_capacity
+    read_capacity   = var.read_capacity
   }
 
   write_capacity = var.write_capacity
