ad27b22f8956ccac0c5699a0efdbeb3dbac3ab04
Alexandre Nunes Reys <anr.alexandre@gmail.com>
2023-11-05 23:40:41 -0300

    create services pages

---
+++ anr_console/src/pages/Services.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import { parseCookies } from 'nookies'
+import { GetServerSideProps } from 'next'
+import { getAPIClient } from '../services/axios'
+import Services from '../components/services/form'
+import Nav from '../components/nav'
+
+export default function ServicesPage() {
+  return (
+    <div>
+      <Head>
+        <title>Usuários</title>
+      </Head>
+      <Nav />
+      <Services />
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const apiClient = getAPIClient(ctx);
+  const { 'nextauth.token': token } = parseCookies(ctx);
+  if (!token) {
+    return { redirect: { destination: '/', permanent: false, } }
+  }
+  return { props: {} }
+}
---
+++ anr_console/src/pages/ServicesList.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import { parseCookies } from 'nookies'
+import { GetServerSideProps } from 'next'
+import { getAPIClient } from '../services/axios'
+import ServicesListComponent from '../components/services/list'
+import Nav from '@/components/nav'
+
+export default function ServicesList() {
+  return (
+    <>
+      <Head>
+        <title>Lista de Organizações</title>
+      </Head>
+      <Nav />
+      <ServicesListComponent />
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const apiClient = getAPIClient(ctx);
+  const { 'nextauth.token': token } = parseCookies(ctx);
+  if (!token) {
+    return { redirect: { destination: '/', permanent: false, } }
+  }
+  return { props: {} }
+}
