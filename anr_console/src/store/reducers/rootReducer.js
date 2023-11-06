475eb1c4d96a7b53cada86ffd619b9ed79fa88ab
Alexandre Nunes Reys <anr.alexandre@gmail.com>
2023-11-05 21:47:49 -0300

    create servicesReducer

--- anr_console/src/store/reducers/rootReducer.js
+++ anr_console/src/store/reducers/rootReducer.js
@@ -2,10 +2,12 @@
 import { combineReducers } from 'redux';
 import usersReducer from './usersSlice';
 import organizationsReducer from './organizationsSlice';
+import servicesReducer from './servicesSlice';
 
 const rootReducer = combineReducers({
   users: usersReducer,
   organizations: organizationsReducer,
+  services: servicesReducer,
 });
 
 export default rootReducer;
