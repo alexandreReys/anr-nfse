909ea8187920a72c2bc15ab560b3765d840cc003
Alexandre Nunes Reys <anr.alexandre@gmail.com>
2023-11-11 10:04:22 -0300

    fix price format and add organizationId to schema

--- anr_console/src/components/services/form/schema.tsx
+++ anr_console/src/components/services/form/schema.tsx
@@ -2,17 +2,24 @@ import { z } from 'zod';
 
 export const schemaForm = z.object({
   service: z.object({
+    organizationId: z.string(),
     id: z.string(),
     description: z.string().min(2, 'Informe a Descrição'),
-    price: z.string()
-      .transform((value) => parseFloat(value))
-      .refine((value) => !isNaN(value) && value >= 0, {
-        message: "Preço deve ser um número válido e não negativo",
-      }),
+    price: z.union([
+      z.string()
+        .transform((value) => parseFloat(value))
+        .refine((value) => !isNaN(value) && value >= 0, {
+          message: "Preço deve ser um número válido e não negativo",
+        }),
+      z.number()
+        .nonnegative("Preço deve ser um número não negativo")
+    ]),
+
     additionalRemarks: z.string(),
   })
 }).transform((field) => ({
   service: {
+    organizationId: field.service.organizationId,
     id: field.service.id,
     description: field.service.description,
     price: field.service.price,
